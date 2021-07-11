7fd0346d124847a1f3459fbc5a9a8e7d153ad8cf
revhea2 <revhea2@gmail.com>
2021-07-11 14:39:36 +0800

    move down the id route and typo in request

--- backend/routes/routes.project.js
+++ backend/routes/routes.project.js
@@ -6,16 +6,16 @@ const projectRoutes = express.Router();
 
 // projectRoutes.get("/", verify, (req, res) => ProjectController.getAllProjects(req, res));
 // projectRoutes.post("/add", verify, (req, res) => ProjectController.createProject(req, res));
-// projectRoutes.get("/:id", verify,  (req, res) => ProjectController.getProject(req, res));
 // projectRoutes.get("/user", verify, (req, res) => ProjectController.getProjectByUserId(req, res));
 // projectRoutes.put("/update", verify, (req, res) => ProjectController.updateProject(req, res));
+// projectRoutes.get("/:id", verify,  (req, res) => ProjectController.getProject(req, res));
 
 
 // debug
 projectRoutes.get("/", (req, res) => ProjectController.getAllProjects(req, res));
 projectRoutes.post("/add", (req, res) => ProjectController.createProject(req, res));
-projectRoutes.get("/:id", (req, res) => ProjectController.getProject(req, res));
 projectRoutes.get("/user", (req, res) => ProjectController.getProjectByUserId(req, res));
 projectRoutes.put("/update", (req, res) => ProjectController.updateProject(req, res));
+projectRoutes.get("/:id", (req, res) => ProjectController.getProject(req, res));
 
 module.exports = projectRoutes;
